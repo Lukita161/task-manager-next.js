b4f9dbc41da757f2626e42afd0c7ce4aa3a7fc23
Luca Seara <caularase@gmail.com>
2024-09-18 10:38:01 -0300

    Finished

--- app/template.tsx
+++ app/template.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function Template({ children }: { children: React.ReactNode }) {
     useEffect(()=> {
-        if(typeof window !== undefined) {
+        if(typeof window !== 'undefined') {
             const isDark = localStorage.getItem('darkMode')
             if(isDark === 'dark') {
                 document.documentElement.classList.add('dark')
--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -4,19 +4,19 @@ import { useCallback, useState } from "react";
 
 export const useDarkMode = () => {
   let darkIsActive = false
-  if(typeof window !== undefined){
+  if(typeof window !== 'undefined'){
     darkIsActive = localStorage.getItem("darkMode") ? true : false
   }
   const [darkMode, setDarkMode] = useState(darkIsActive);
   const changeMode = useCallback(() => {
     setDarkMode(!darkMode);
     if (darkMode === false) {
-      if(typeof window !== undefined) {
+      if(typeof window !== 'undefined') {
         localStorage.setItem("darkMode", "day");
       }
       document.documentElement.classList.remove("dark");
     } else {
-      if(typeof window !== undefined) {
+      if(typeof window !== 'undefined') {
         localStorage.setItem("darkMode", "dark");
       }
       document.documentElement.classList.add("dark");
